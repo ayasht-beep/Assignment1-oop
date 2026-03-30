ebce45e56aba91eca5b11ed730db576e4e58a7dd
idoza <zaigi@post.bgu.ac.il>
2026-03-30 14:27:33 +0300

    new

--- src/Polynomials/Monomial.java
+++ src/Polynomials/Monomial.java
@@ -1,5 +1,5 @@
 package Polynomials;
-
+import Scalars.IntegerScalar;
 import Scalars.Scalar;
 
 public class Monomial {
@@ -11,28 +11,51 @@ public class Monomial {
         this.coefficient = coefficient;
     }
 
+    public int getExponent() {
+        return exponent;
+    }
+    public Scalar getCoefficient(){
+        return coefficient;
+    }
+
     public Monomial add(Monomial m){
-        if (this.exponent != m.getExponent) {
+        if (this.exponent != m.getExponent()) {
             return null;
         }
-        Scalar newCoefficient = this.coefficient.add(m.getCoefficient);
+        Scalar newCoefficient = this.coefficient.add(m.getCoefficient());
         return new Monomial(this.exponent, newCoefficient);
     }
 
-    public Monomial mul(Monomial m){return null;}
+    public Monomial mul(Monomial m){
+        int newExp = getExponent() + m.getExponent();
+        Scalar newCoefficient = getCoefficient().mul(m.getCoefficient());
+        return new Monomial(newExp, newCoefficient);
+    }
 
-    public Scalar evulate(Scalar s){
+    public Scalar evaluate(Scalar s){
         Scalar sPower = s.power(this.exponent);
         return this.coefficient.mul(sPower);
     }
 
-    public Monomial derivative(){return null;}
+    public Monomial derivative(){
+        if (getExponent() == 0) {
+            return new Monomial(0, new IntegerScalar(0));
+        }
+        return new Monomial(getExponent() - 1, getCoefficient().mul(new IntegerScalar(getExponent())));
+    }
+
 
     public int sign(){
         return this.coefficient.sign();
     }
 
-    public boolean equals(Object o){return false;}
+    public boolean equals(Object o){
+        if (! (o instanceof Monomial)) {
+            return false;
+        }
+        Monomial other = (Monomial) o;
+        return getCoefficient().equals(other.getCoefficient()) && getExponent() == other.getExponent();
+    }
 
     public String toString(){
         String coefficientStr = this.coefficient.toString();
