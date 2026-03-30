3f97819d2f52c5970a07cb851771c97f80bbb182
ayasht-beep <ayasht@post.bgu.ac.il>
2026-03-30 13:56:48 +0300

    .

--- src/Monomial.java
+++ src/Monomial.java
@@ -9,13 +9,50 @@ public class Monomial {
         this.coefficient = coefficient;
     }
 
-    public Monomial add(Monomial m){return null;}
+    public Monomial add(Monomial m){
+        if (this.exponent != m.getExponent) {
+            return null;
+        }
+        Scalar newCoefficient = this.coefficient.add(m.getCoefficient);
+        return new Monomial(this.exponent, newCoefficient);
+    }
+
     public Monomial mul(Monomial m){return null;}
-    public Scalar evulate(Scalar s){return null;}
+
+    public Scalar evulate(Scalar s){
+        Scalar sPower = s.power(this.exponent);
+        return this.coefficient.mul(sPower);
+    }
+
     public Monomial derivative(){return null;}
-    public int sign(){return 0;}
+
+    public int sign(){
+        return this.coefficient.sign();
+    }
+
     public boolean equals(Object o){return false;}
-    public String toString(){return null;}
+    public String toString(){
+        String coefficientStr = this.coefficient.toString();
+        if(this.exponent == 0){
+            return coefficientStr;
+        }
+        String result = "";
+        if(coefficientStr.equals("1")){
+            result = "";
+        }
+        else if(coefficientStr.equals("-1")){
+            result = "-";
+        }
+        else{
+            result = coefficientStr;
+        }
+        if(this.exponent == 1){
+            return result + "x";
+        }
+        else{
+            return result + "x^" + this.exponent;
+        }
+    }
 
 
 }
